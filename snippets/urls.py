066b43a51a9f56c7255f4ce5b497cc469253d99c
truc0 <22969604+truc0@users.noreply.github.com>
2022-04-04 20:18:59 +0800

    feat: add link to detail pages

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import index, detail
 
+app_name = 'snippets'
 urlpatterns = [
     path('', index, name='index'),
     path('<int:pk>', detail, name='detail'),
